f41d59cc007e86e8587b946139fc1f61ac0a13ea
Kamil <kamil.maciejewski@hotmail.com>
2021-02-23 19:08:04 +0100

    HelloMessageListener implementation

--- src/main/java/com/km/jmsdemo/listener/HelloMessageListener.java
+++ src/main/java/com/km/jmsdemo/listener/HelloMessageListener.java
@@ -1,2 +1,23 @@
-package com.km.jmsdemo.listener;public class HelloMessageListener {
+package com.km.jmsdemo.listener;
+
+import com.km.jmsdemo.config.JmsConfig;
+import com.km.jmsdemo.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) {
+
+        System.out.println("I got a message!!!");
+        System.out.println(helloWorldMessage);
+    }
 }
