faf98b0515bc17f061cc2a6f3b44cf61352a9a81
Kamil <kamil.maciejewski@hotmail.com>
2021-02-23 18:51:25 +0100

    Added HelloSender class responsible for sending message to MQ

--- src/main/java/com/km/jmsdemo/sender/HelloSender.java
+++ src/main/java/com/km/jmsdemo/sender/HelloSender.java
@@ -1,2 +1,31 @@
-package com.km.jmsdemo.sender;public class HelloSender {
+package com.km.jmsdemo.sender;
+
+import com.km.jmsdemo.config.JmsConfig;
+import com.km.jmsdemo.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage() {
+        System.out.println("I am sending a message");
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello World!")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message send!");
+    }
 }
